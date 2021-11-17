f45e651ba2129d73413b4bfa9ab8d221c8d5acda
Corbyyy <anboakye004@st.ug.edu.gh>
2021-11-17 20:12:11 +0800

    Initial commit

---
+++ src/consts/perfumes.js
@@ -0,0 +1,49 @@
+const perfumes = [
+    {
+      id: '1',
+      name: 'Red Diamond',
+      image: require('../assets/img1.jpg'),
+      price: '$500000',
+      liked: true,
+    },
+    {
+      id: '2',
+      name: 'Chanel',
+      image: require('../assets/img2.jpg'),
+      price: '$400000',
+      liked: false,
+    },
+    {
+      id: '3',
+      name: 'Park Avenue',
+      image: require('../assets/img3.jpg'),
+      price: '$200000',
+      liked: true,
+    },
+    {
+      id: '4',
+      name: 'Chanel',
+      image: require('../assets/img5.jpg'),
+      price: '$150000',
+      liked: true,
+    },
+
+    {
+      id: '3',
+      name: 'Versace',
+      image: require('../assets/img6.jpg'),
+      price: '$100000',
+      liked: true,
+    },
+    {
+      id: '4',
+      name: 'Chanel',
+      image: require('../assets/img4.jpg'),
+      price: '$100000',
+      liked: true,
+    },
+   
+  ];
+  
+  export default perfumes;
+  
